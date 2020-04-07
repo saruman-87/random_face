e5ab01fccb1a6b10b3b1c776c794f49f8cb71f2e
SaRuMan <ruslan.soltaev@gmail.com>
2020-04-07 12:51:39 +0300

    Убрал закрытие файла

--- face.rb
+++ face.rb
@@ -3,23 +3,16 @@
 # Программа-фоторобот, которая «собирает» лицо из фрагментов.
 
 # Заведем четыре переменные с путями к файлам с набором для каждого фрагмента.
-# Обратите внимание, что файлы с набором фрагментов нужно создавать папке data,
-# которая находится в той же папке, где находится программа.
 path ="#{__dir__}/data/parts_of_face/"
 foreheads_path = "#{path}foreheads.txt"
 eyes_path = "#{path}eyes.txt"
 noses_path = "#{path}noses.txt"
 mouths_path = "#{path}mouths.txt"
 
-#Голова
 foreheads = File.new(foreheads_path, "r:UTF-8").readlines
-file.close
 eyes = File.new(eyes_path, "r:UTF-8").readlines
-file.close
 noses = File.new(noses_path, "r:UTF-8").readlines
-file.close
 mouths = File.new(mouths_path, "r:UTF-8").readlines
-file.close
 face = [foreheads.sample, eyes.sample, noses.sample, mouths.sample]
 
 #Определяем текущую дату
